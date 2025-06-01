17a726f0787738297c89cf86389c686435ee7ec8
Yuri Cainã <yuricaina@gmail.com>
2025-05-31 21:58:04 -0300

    feat: frontend criado com Vite + React

--- frontend/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-
-const App = () => {
-  return <h1>OctoClinic Frontend</h1>;
-};
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
